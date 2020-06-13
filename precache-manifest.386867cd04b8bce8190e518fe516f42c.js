b4bb44f485e827112ed62000d4ea2aeb714b0550
Daniel <amenou.daniel@gmail.com>
2020-06-13 11:59:10 +0300

    Updates

---
+++ precache-manifest.386867cd04b8bce8190e518fe516f42c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "979ebb094b67b2840851ee677a597ca6",
+    "url": "/Daniel-Amenou-11-06-2020/index.html"
+  },
+  {
+    "revision": "c0717a4ad077eb570b68",
+    "url": "/Daniel-Amenou-11-06-2020/static/css/2.a2026422.chunk.css"
+  },
+  {
+    "revision": "c0717a4ad077eb570b68",
+    "url": "/Daniel-Amenou-11-06-2020/static/js/2.439f7ce3.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/Daniel-Amenou-11-06-2020/static/js/2.439f7ce3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24ddcd22c5da0baad5e5",
+    "url": "/Daniel-Amenou-11-06-2020/static/js/main.5c43dbf0.chunk.js"
+  },
+  {
+    "revision": "52855f25190040424a8a",
+    "url": "/Daniel-Amenou-11-06-2020/static/js/runtime-main.f50d8f22.js"
+  }
+]);
