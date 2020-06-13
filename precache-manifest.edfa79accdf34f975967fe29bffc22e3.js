034a964536749a9c3492b498264a817063bc922d
Daniel <amenou.daniel@gmail.com>
2020-06-13 19:13:17 +0300

    Updates

--- precache-manifest.edfa79accdf34f975967fe29bffc22e3.js
+++ precache-manifest.edfa79accdf34f975967fe29bffc22e3.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "94d1f7dbe7ea9acc2b7a672641065005",
+    "revision": "010309f841c213232c1861e413c805ba",
     "url": "/Daniel-Amenou-11-06-2020/index.html"
   },
   {
-    "revision": "e25e246c2160fae6fbc0",
+    "revision": "a3956f5fb608cab8b015",
     "url": "/Daniel-Amenou-11-06-2020/static/css/2.a2026422.chunk.css"
   },
   {
-    "revision": "e25e246c2160fae6fbc0",
-    "url": "/Daniel-Amenou-11-06-2020/static/js/2.af02afba.chunk.js"
+    "revision": "a3956f5fb608cab8b015",
+    "url": "/Daniel-Amenou-11-06-2020/static/js/2.2e1ea765.chunk.js"
   },
   {
     "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
-    "url": "/Daniel-Amenou-11-06-2020/static/js/2.af02afba.chunk.js.LICENSE.txt"
+    "url": "/Daniel-Amenou-11-06-2020/static/js/2.2e1ea765.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "0b8a4e759c9805735382",
-    "url": "/Daniel-Amenou-11-06-2020/static/js/main.a6f55162.chunk.js"
+    "revision": "2ca2560d20c36041286c",
+    "url": "/Daniel-Amenou-11-06-2020/static/js/main.9fbb3377.chunk.js"
   },
   {
     "revision": "52855f25190040424a8a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Daniel-Amenou-11-06-2020/precache-manifest.804852e6f2bcdf13321c9385991fc85d.js"
+  "/Daniel-Amenou-11-06-2020/precache-manifest.edfa79accdf34f975967fe29bffc22e3.js"
 );
 
 self.addEventListener('message', (event) => {
